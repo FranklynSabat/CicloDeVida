0ff616c9324f3e1783984e5b668b29d2ca7e1ada
FranklynSabat <franklynsabat@gmail.com>
2017-12-10 16:12:40 -0200

    Ciclo de vida de uma activity
    Ensina o como funciona o ciclo de vida de uma activity, trabalhar com as cores de uma activite para outra e tambem navegação entre elas.

---
+++ CicloDeVida/app/src/main/java/br/usjt/ciclodevida/Activity_c.java
@@ -0,0 +1,13 @@
+package br.usjt.ciclodevida;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity_c extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_c);
+    }
+}
